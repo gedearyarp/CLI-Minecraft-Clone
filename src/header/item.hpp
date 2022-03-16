386d30b5487d962ccc089ce1612237181dfeb66a
dhikaarta <16520019@std.stei.itb.ac.id>
2022-03-16 11:28:42 +0700

    added new attribute (category) to item and implemented nontool item

--- src/header/item.hpp
+++ src/header/item.hpp
@@ -1,37 +1,49 @@
 #ifndef __ITEM_H
 #define __ITEM_H
 #include <string>
+#define MAX_NONTOOL 64
 
 using namespace std;
 
-class item{
+class item
+{
 protected:
     int id;
     string name;
-    string type;    //LOG, PLANK, STONE, - 
+    string type;     // LOG, PLANK, STONE, -
+    string category; // TOOL & NONTOOL
+
 public:
     item();
-    item(int id, string name, string type);
+    item(int id, string name, string type, string category);
     item(string name);
     int get_id();
     string get_name();
+    virtual void itemInfo() const;
 };
 
-class tool : virtual public item{
+class tool : virtual public item
+{
 private:
     int durability;
+
 public:
     tool();
     tool(int id, string name, int durability);
     tool(string name);
     void use();
+    void itemInfo() const;
 };
 
-// class nontool : virtual public item{
-// private:
-//     int quantity;
-// public:
-//     nontool();
-//     nontool(int id, string name, int quanitity);
-// };
+class nontool : virtual public item
+{
+private:
+    int quantity;
+
+public:
+    nontool();
+    nontool(int id, string name, string type, int quantity);
+    void itemInfo() const;
+};
+
 #endif
--- src/implementation/item.cpp
+++ src/implementation/item.cpp
@@ -1,41 +1,79 @@
 #include "../header/item.hpp"
+#include <iostream>
+using namespace std;
 
-item::item(){
-    this->id=0;
-    this->name="-";
-    this->type="type";
+item::item()
+{
+    this->id = 0;
+    this->name = "-";
+    this->type = "type";
+    this->category = "-";
 }
-item::item(int id, string name, string type){
-    this->id=id;
-    this->name=name;
-    this->type=type;
+item::item(int id, string name, string type, string category)
+{
+    this->id = id;
+    this->name = name;
+    this->type = type;
+    this->category = category;
 }
-item::item(string name){
+item::item(string name)
+{
     // nyari tau id dan tipe dari config file
 }
 
-int item::get_id(){
+int item::get_id()
+{
     return this->id;
 }
 
-string item::get_name(){
+string item::get_name()
+{
     return this->name;
 }
 
-
-tool::tool() : item(0,"name","-"){
-    this->durability=10;
+tool::tool() : item(0, "name", "-", "TOOL")
+{
+    this->durability = 10;
 }
-tool::tool(int id, string name, int durability) : item(id,name,"-"){
-    this->durability=durability;
+tool::tool(int id, string name, int durability) : item(id, name, "-", "TOOL")
+{
+    this->durability = durability;
 }
-tool::tool(string name) : item(name){
-    this->durability=10;
+tool::tool(string name) : item(name)
+{
+    this->durability = 10;
 }
-void tool::use(){
+void tool::use()
+{
     this->durability--;
-    if (this->durability <= 0){
+    if (this->durability <= 0)
+    {
         // Ilang dari inventory
     }
 }
 
+void tool::itemInfo() const
+{
+    cout << "ID :" << this->id << endl;
+    cout << "Name :" << this->name << endl;
+    cout << "Type :" << this->type << endl;
+    cout << "Durability :" << this->durability << endl;
+}
+
+nontool::nontool() : item(0, "-", "-", "-")
+{
+    this->quantity = 0;
+}
+
+nontool::nontool(int id, string name, string type, int quantity) : item(id, name, type, "NONTOOL")
+{
+    this->quantity = quantity;
+}
+
+void nontool::itemInfo() const
+{
+    cout << "ID :" << this->id << endl;
+    cout << "Name :" << this->name << endl;
+    cout << "Type :" << this->type << endl;
+    cout << "Quantity :" << this->quantity << endl;
+}
