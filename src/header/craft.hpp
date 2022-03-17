a1c855aba17c30bd62eb06c6440533c827aee181
graceclaudia19 <graceclaudia78@gmail.com>
2022-03-18 04:55:52 +0700

    add read file implementation for crafting and supporting function for recipe

--- src/header/craft.hpp
+++ src/header/craft.hpp
@@ -2,64 +2,36 @@
 #define CRAFT_HPP
 
 #include "item.hpp"
+#include "Recipes.hpp"
 
 // STL
 #include <string>
 #include <map>
 
-using namespace std;
-
-class CraftingTableSlot {
-    private:
-        Item item;
-        int quantity;
-    public:
-        CraftingTableSlot();
-
-        // add item to slot
-        void AddItemToCraftingTableSlotByItem(Item item, int qty);
-        void AddItemToCraftingTableSlotByName(string itemName, int qty);
-
-        // add and substract quantity of certain exist item from crafting table slot
-        void AddItemQuantity(int qty);
-        void DecreaseItemQuantity(int qty);
-        
-        // getters
-        int getQuantity();
-        Item getItem();
-        
-};
-
 class CraftingTable {
     private:
-        CraftingTableSlot TableSlot[3][3];
-        map<int, vector<string>> recipes;
+        Item Table[3][3];
+        Recipes recipes;
     public:
         // Create table of 9 x 9 matrix 
         CraftingTable();
 
         // Counting item on crafting table
-        int CountItemOnTable();
+        int countItemOnTable();
 
-        // 4. MOVE <INVENTORY_SLOT_ID> N <CRAFTING_SLOT_ID_1> <CRAFTING_SLOT_ID_2> ...
-        // MOVE I0 N C0 C1 C2 ... CN
-        void PutItemToCraftingTableFromInventory(string craftingSlotId, int amount, string InventorySlotId);
+        bool CraftingTable::isSameAsRecipe(SingleRecipe mirroredRecipe) const;
 
         // check recipe
-        bool EligibleForCrafting() const;
+        bool eligibleForCrafting() const;
 
         // CRAFT -> di implementasi, panggil GIVE
-        void Craft();
+        void craft();
         // ngecek ada yg sesuai recipe ga
         
-        // 6. MOVE <CRAFTING_SLOT_ID> 1 <INVENTORY_SLOT_ID>
-        // MOVE I0 1 I1
-        void PutItemToInventoryFromCraftingTable(string InventorySlotId, int amount, string craftingSlotId);
-        
         // melihat table crafting
-        void ShowCraftingTable();
+        void showCraftingTable();
 
-        void ReadRecipe();
+        void readRecipes();
 };
 
 #endif
---
+++ src/header/recipes.hpp
@@ -0,0 +1,80 @@
+#ifndef RECIPES_HPP
+#define RECIPES_HPP
+
+#include "item.hpp"
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class SingleRecipe {
+    private:
+        string filename;
+        int NRowRecipe;
+        int NColRecipe;
+        string ItemResultName;
+        int ItemResultQuantity;
+        vector<vector<Item>> ItemPlacement;
+        // array of array
+        // 
+        //     [[Plank], 
+        //     [Plank]]
+        // 
+    
+        
+    public:
+        SingleRecipe();
+        SingleRecipe(string filename, int NrowRecipe, int NcolRecipe, string ItemResultName, int ItemResultQuantity, vector<vector<Item>> ItemPlacement);
+
+        int totalSlotNeededForCraft();
+        
+        // getters
+        string getFilename();
+        int getNRowRecipe();
+        int getNColRecipe();
+        string getItemResultName();
+        int getItemResultQuantity();
+        vector<vector<Item>> getItemPlacement();
+
+        // mirror of itemplacement;
+        SingleRecipe getItemMirroredInPlacement(SingleRecipe originalRecipe);
+
+        // checking if mirrored matrix have the same recipe as the recipe
+        bool isOriginalRecipeSameAsMirroredRecipe(SingleRecipe mirroredRecipe);
+
+        void showItemPlacement();
+
+};
+
+class Recipes {
+    private:
+        vector<SingleRecipe> recipesList;
+        int totalRecipe;
+    public:
+        Recipes();
+
+        int getTotalRecipe();
+        void setTotalRecipe(int totalRecipe);
+        vector<SingleRecipe> getRecipesList();
+
+        void AddNewSingleRecipe(SingleRecipe newSingleRecipe);
+
+        // {
+        //     read directory
+        //     for i in listOfFiles:
+        //         read(i);
+                
+        //         line1 = 1 1;
+        //         NRowRecipe = line1[0];
+        //         NColRecipe = line1[2];
+                        
+        //         slot = RecipeSlot(NRowRecipe, NColRecipe);
+        //         for i in range(Nrow):
+        //             for j in range(ncol):
+        //                 recipeSlot.itemplacement[i][j] = getline
+                
+        // }
+};
+
+#endif
